a3d96e08242d1117ca5458745162e6efcaa1f461
Shreyesh <imshreyesh17@gmail.com>
2019-10-19 04:17:36 -0400

    firebase set up

---
+++ DevNet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevNet
+//
+//  Created by Shreyesh on 10/19/19.
+//  Copyright © 2019 Quark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
