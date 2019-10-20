c2a551a0f6b914db24d9da2b2f7151254dfa7736
Shreyesh <imshreyesh17@gmail.com>
2019-10-20 06:26:49 -0400

    data from database

--- DevNet/View/Developer.swift
+++ DevNet/View/Developer.swift
@@ -9,6 +9,7 @@ class Developer: UIViewController{
     
     @IBOutlet weak var imageOfDev: UIImageView!
     @IBOutlet weak var devName: UILabel!
+    @IBOutlet weak var universityNameField: UILabel!
     
     
     var ref: DocumentReference? = nil
@@ -23,15 +24,24 @@ class Developer: UIViewController{
         imageOfDev.image=UIImage(named: "Some_Guy_On_A_Potato_grande.jpg")
         
     }
+    
+    //tagListView.removeAllTags()
+    
     @IBAction func acceptButtonPressed(_ sender: Any) {
         let randomNumber = arc4random_uniform(4) + 1
-        ref=db.collection("cities").document(String(randomNumber))
+        print(randomNumber)
+        ref=db.collection("students").document(String(randomNumber))
         
         ref?.getDocument { (document, err) in
-            if case let err=err{
+            if let err=err{
                 print(err)
             } else {
-                print(document?.data())
+                self.devName.text=document?.get("name") as! String?
+            self.universityNameField.text=document?.get("university") as! String?
+                
+                self.tagView.removeAllTags()
+                self.tagView.addTags(document?.get("skills") as! Array<String>)
+                
             }
         }
         
